83a24de5f7c65c21945e2535743c7dbe15c68e8e
naveen3039 <81296692+naveen3039@users.noreply.github.com>
2021-03-24 17:34:52 +0530

    Update msg.py

--- msg.py
+++ msg.py
@@ -9,7 +9,7 @@ mail = Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'kumarnaveen.kumar3039@gmail.com'
+app.config['MAIL_USERNAME'] = ''
 app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
